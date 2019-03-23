508c7f91c3368983a6c841ebab20b150e6f9c512
asu <asu@163.com>
2019-03-23 21:17:44 +0800

    zuul 服务网关转发

---
+++ zuul-service/src/main/java/com/asu/zuul/service/ZuulServiceApplication.java
@@ -0,0 +1,15 @@
+package com.asu.zuul.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServiceApplication.class, args);
+	}
+
+}
