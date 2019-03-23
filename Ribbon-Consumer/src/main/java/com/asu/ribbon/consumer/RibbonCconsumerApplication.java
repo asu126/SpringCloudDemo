38ecad09ad84ecc43c6da282103b31fdd834302c
asu <asu@163.com>
2019-03-23 19:07:07 +0800

    服务提供者 Eureka + 服务消费者 Feign + 断路器监控Hystrix

--- Ribbon-Consumer/src/main/java/com/asu/ribbon/consumer/RibbonCconsumerApplication.java
+++ Ribbon-Consumer/src/main/java/com/asu/ribbon/consumer/RibbonCconsumerApplication.java
@@ -7,6 +7,9 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@EnableHystrix
 @EnableDiscoveryClient
 @SpringBootApplication
 public class RibbonCconsumerApplication {
--- Ribbon-Consumer/src/main/java/com/asu/ribbon/consumer/controller/ConsumerController.java
+++ Ribbon-Consumer/src/main/java/com/asu/ribbon/consumer/controller/ConsumerController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 /**
  * 描述:调用提供者的 `home` 方法
  *
@@ -16,14 +18,19 @@ public class ConsumerController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "defaultStores")
     @GetMapping(value = "/hello")
     public String hello() {
         return restTemplate.getForEntity("http://eureka-provider/", String.class).getBody();
     }
 
+    @HystrixCommand(fallbackMethod = "defaultStores")
     @GetMapping(value = "/hello123")
     public String hello123() {
         return restTemplate.getForEntity("http://eureka-provider/123", String.class).getBody();
     }
-}
 
+    public String defaultStores() {
+        return "Ribbon + hystrix ,提供者服务挂了";
+    }
+}
